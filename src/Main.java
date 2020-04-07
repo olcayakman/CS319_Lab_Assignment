2f32ea3769f3364346b156ae56f80bda6607cd9c
olcay <olcayakman@hotmail.com>
2020-04-07 12:21:47 +0300

    Game is implemented in Main class.

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,45 @@ public class Main {
 
         ArrayList<Integer> sorted = ao.concatAndSort(list1, list2, list3);
 
-        System.out.println(sorted);
+        System.out.println("Result => " + sorted);
+
+        System.out.println("==================================");
+
+        Player p1, p2, p3, p4;
+
+        p1 = new Player("Tom");
+        p2 = new Player ("John");
+        p3 = new Player ("James");
+        p4 = new Player("Henry");
+
+        Die die = new Die();
+
+        for (int i = 0; i < 5; i++) {
+            int dieValue = die.rollDie();
+            p1.addScore(dieValue);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            int dieValue = die.rollDie();
+            p2.addScore(dieValue);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            int dieValue = die.rollDie();
+            p3.addScore(dieValue);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            int dieValue = die.rollDie();
+            p4.addScore(dieValue);
+        }
+
+        System.out.println(p1.getName() + " has " + p1.getScore() + " points.");
+        System.out.println(p2.getName() + " has " + p2.getScore() + " points.");
+        System.out.println(p3.getName() + " has " + p3.getScore() + " points.");
+        System.out.println(p4.getName() + " has " + p4.getScore() + " points.");
+
+
 
     }
 
